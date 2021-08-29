31a2c4696f8035e53337436b6e53771a44776cf1
6sRyuSK <6sakarsei.gamer@gmail.com>
2021-08-29 14:12:38 +0900

    create Resource Favorite

---
+++ app/favorites/mutations/createFavorite.ts
@@ -0,0 +1,14 @@
+import { resolver } from "blitz"
+import db from "db"
+import { z } from "zod"
+
+const CreateFavorite = z.object({
+  name: z.string(),
+})
+
+export default resolver.pipe(resolver.zod(CreateFavorite), resolver.authorize(), async (input) => {
+  // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+  const favorite = await db.favorite.create({ data: input })
+
+  return favorite
+})

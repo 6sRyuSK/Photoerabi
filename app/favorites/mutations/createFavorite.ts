38a940b0598ea079f7180ec6ccc1077234e5a18f
6sRyuSK <6sakarsei.gamer@gmail.com>
2021-08-29 14:17:05 +0900

    modify createFavorite.ts

--- app/favorites/mutations/createFavorite.ts
+++ app/favorites/mutations/createFavorite.ts
@@ -4,6 +4,7 @@ import { z } from "zod"
 
 const CreateFavorite = z.object({
   name: z.string(),
+  userId: z.number(),
 })
 
 export default resolver.pipe(resolver.zod(CreateFavorite), resolver.authorize(), async (input) => {
